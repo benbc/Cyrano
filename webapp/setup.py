02f07f1e40f9b66dea14fd88145b210b6d979901
Ben Butler-Cole <ben@bridesmere.com>
2012-01-14 19:36:20 +0000

    Clean up cruft from skeleton.

--- webapp/setup.py
+++ webapp/setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
@@ -19,17 +13,12 @@ requires = [
 setup(name='cyrano',
       version='0.0',
       description='cyrano',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+      long_description='',
+      classifiers=[],
       author='',
       author_email='',
       url='',
-      keywords='web wsgi bfg pylons pyramid',
+      keywords='',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
