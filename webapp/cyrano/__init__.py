196a0e620abfe1bb35537b29c01b4df3e882998f
Ben Butler-Cole <ben@bridesmere.com>
2012-03-18 20:42:55 +0000

    Fix ordering of route definitions.

--- webapp/cyrano/__init__.py
+++ webapp/cyrano/__init__.py
@@ -6,8 +6,8 @@ def main(global_config, **settings):
 
     config.add_static_view('static', 'static')
     config.add_route('front', '/')
-    config.add_route('album', '/album/{id}')
     config.add_route('add_album', '/album/add', request_method='POST')
+    config.add_route('album', '/album/{id}')
     config.add_route('add_video', '/video/add', request_method='POST')
 
     config.scan()
